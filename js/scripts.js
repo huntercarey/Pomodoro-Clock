708069750d60c47abd53e17e34edc98ccc00ccf6
Hunter Carey <huntercarey902@gmail.com>
2019-10-29 10:55:31 -0500

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -49,7 +49,9 @@ function Clock() {
         mode = 'Session', //Keeps track of what mode we're in (session or break)
         active = false, //Keeps track of whether the clock is running or not
         _this = this, //Refeerence to the clock itself
-        timer; //Reference to the interval that we set up to make the timer run
+        timer,  //Reference to the interval that we set up to make the timer run
+        startAudio = new Audio('../assets/start.mp3'),
+        endAudio = new Audio('../assets/end.mp3');
 
         //DISPLAY FUNCTIONS
 
@@ -169,6 +171,7 @@ function Clock() {
             if (sessionCount === 0) {
                 sessionCount = 1;
                 this.displaySessionCount();
+                startAudio.play();
             }
             $('.time-start').text('Pause');
             timer = setInterval(function() {
@@ -192,12 +195,14 @@ function Clock() {
                     currentTime = breakTime;
                     startTime = breakTime;
                     this.displaySessionCount();
+                    endAudio.play();
                 } else {
                     mode = 'Session';
                     currentTime = sessionTime;
                     startTime = sessionTime;
                     sessionCount += 1;
                     this.displaySessionCount();
+                    startAudio.play();
                 }
             }
         }
